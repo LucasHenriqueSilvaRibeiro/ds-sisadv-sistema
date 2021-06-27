77f82743e36a0e1a58a94427b6b2f78960137930
Lucas Henrique Silva Ribeiro <lucasheenrique92920239@gmail.com>
2021-06-26 23:14:24 -0300

    Tela Principal - Adições de botões e mudanças no código referente a grid

---
+++ Dominio/Servico.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SisAdv.Dominio
+{
+    public class Servico
+    {
+        public int Id { get; set; }
+
+        public String Cliente { get; set; }
+
+        public double Valor { get; set; }
+        
+        public String Descricao { get; set; }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,6 +25,26 @@ namespace SisAdv
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<Dominio.Servico> listaServicos = new List<Dominio.Servico>();
+
+            for (int i = 0; i < 30; i++)
+            {
+                listaServicos.Add(new Dominio.Servico()
+                {
+                    Id = i + 1,
+                    Cliente = "Clientão - " + i,
+                    Valor = 5 * i,
+                    Descricao = "Caso Civil" + 1,
+                });
+            }
+
+            //Está dando um erro nesta linha, descobrir o que é!!
+            dataGridServicosRecentes.ItemsSource = listaServicos;
         }
 
         private void btadicionar_Click(object sender, RoutedEventArgs e)
@@ -43,9 +63,46 @@ namespace SisAdv
             funcoesbuscar.Visibility = Visibility.Visible;
         }
 
-        private void btbuscar_LostFocus(object sender, RoutedEventArgs e)
+
+        private void btaddrevento_Click(object sender, RoutedEventArgs e)
         {
-            funcoesbuscar.Visibility = Visibility.Collapsed;
+
+        }
+
+        private void btImprimirRelatorio_Click(object sender, RoutedEventArgs e)
+        {
+            ImprimirRelatorio imprimirRelatorio = new ImprimirRelatorio();
+
+            imprimirRelatorio.ShowDialog();
+        }
+
+        private void btbuscarcliente_Click(object sender, RoutedEventArgs e)
+        {
+            BuscarCliente buscarCliente = new BuscarCliente();
+
+            buscarCliente.ShowDialog();
+        }
+
+        private void btbuscaservico_Click(object sender, RoutedEventArgs e)
+        {
+            BuscarServico buscarServico = new BuscarServico();
+
+            buscarServico.ShowDialog();
+        }
+
+        private void btbuscardespesalucro_Click(object sender, RoutedEventArgs e)
+        {
+            BuscarDespesaLucro buscarDespesaLucro = new BuscarDespesaLucro();
+
+            buscarDespesaLucro.ShowDialog();
+        }
+
+        private void btsair_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Deseja Realmente Sair?", "Save error", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+                this.Close();
         }
     }
     }
