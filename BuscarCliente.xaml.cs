17881bb021968a6cf61acb84d378f73f305fdabd
Mateus Enrique Bezerra <mh4789mateus@gmail.com>
2021-06-28 00:00:49 -0300

    Tela Buscar Cliente - Alterações de Tamanho, Grid e Masked

--- BuscarCliente.xaml.cs
+++ BuscarCliente.xaml.cs
@@ -22,6 +22,26 @@ namespace SisAdv
         public BuscarCliente()
         {
             InitializeComponent();
+            Loaded += BuscarCliente_Loaded;
+        }
+
+        public void BuscarCliente_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<Cliente> listaCliente = new List<Cliente>();
+
+            for (int i = 0; i < 30; i++)
+            {
+                listaCliente.Add(new Cliente()
+                {
+                    Id = i + 1,
+                    Nome = "Joãozinho - " + i,
+                    Cpf = "304980239-01",
+                    Rg = "0394802",
+                });
+            }
+
+            //Está dando um erro nesta linha, descobrir o que é!!
+            dataGridBuscarCliente.ItemsSource = listaCliente;
         }
     }
 }
---
+++ Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SisAdv
+{
+    public class Cliente
+    {
+        public String Nome { get; set; }
+        public String Cpf { get; set; }
+        public String Rg { get; set; }
+        public int Id { get; set; }
+    }
+}
