3e79a02baa5347ee2323b88e6bcc5c4303d5a029
Mateus Enrique Bezerra <mh4789mateus@gmail.com>
2021-06-28 21:49:39 -0300

    Telas Buscar Cliente, Buscar Lucro e Buscar Despesa - Finalizadas (acredito)

---
+++ BuscarDespesa.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SisAdv
+{
+    /// <summary>
+    /// Lógica interna para BuscarDespesa.xaml
+    /// </summary>
+    public partial class BuscarDespesa : Window
+    {
+        public BuscarDespesa()
+        {
+            InitializeComponent();
+            Loaded += BuscarDespesa_Loaded;
+        }
+
+        public void BuscarDespesa_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<Despesa> listaDespesa = new List<Despesa>();
+
+            for (int i = 0; i < 30; i++)
+            {
+                listaDespesa.Add(new Despesa()
+                {
+                    Data = "20/02/2020",
+                    Origem = "Despesa - " + i,
+                    Mensal = true,
+                    Valor = 20.30 + i,
+                });
+            }
+
+            dataGridBuscarDespesa.ItemsSource = listaDespesa;
+        }
+    }
+}
