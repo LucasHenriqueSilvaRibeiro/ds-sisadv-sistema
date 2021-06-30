1ab4697c0b7857ae62e04f55f84414ca4d89b6ab
João Rafael <Joao_rafaaviador@outlook.com>
2021-06-30 20:57:59 -0300

    Tela Cadastrar Lucro
    Como era a msm o lucas me recomndou pegar da commit do bruno.

---
+++ Cadastrarlucro.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SisAdv
+{
+    /// <summary>
+    /// Lógica interna para Cadastrarlucro.xaml
+    /// </summary>
+    public partial class Cadastrarlucro : Window
+    {
+        public Cadastrarlucro()
+        {
+            InitializeComponent();
+            Loaded += Cadastrarlucro_Loaded;
+        }
+        public void Cadastrarlucro_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<Lucro> listadeLucro = new List<Lucro>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                listadeLucro.Add(new Lucro()
+                {
+                    Valor = i + 10,
+                });
+            }
+            gridcadastrarlucro.ItemsSource = listadeLucro;
+        }
+    }
+}
