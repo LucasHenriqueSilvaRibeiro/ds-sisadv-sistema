a96b1564f2106f5a3a7b09fa1741d1e8f414422e
Kayky Ferreira <kaykywcf@gmail.com>
2021-10-24 23:14:35 -0400

    cadastrar caixa

---
+++ Views/Cadastrarcaixa.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SisAdv.Views
+{
+    /// <summary>
+    /// Lógica interna para Cadastrarcaixa.xaml
+    /// </summary>
+    public partial class Cadastrarcaixa : Window
+    {
+        public Cadastrarcaixa()
+        {
+            InitializeComponent();
+        }
+
+        private void Btnadicionar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnSalvar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btnpesquisarcaixa_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
